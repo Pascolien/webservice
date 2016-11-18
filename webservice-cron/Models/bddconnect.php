882a9fce9d33d81a2f0f7996edd2bd9a57bf7878
Pascolien <prince.matsika@epsi.fr>
2016-11-18 14:20:09 +0100

    Add files via upload

---
+++ webservice-cron/Models/bddconnect.php
@@ -0,0 +1,15 @@
+<?php
+
+try{
+       $bdd = new PDO('mysql:host=192.168.1.199;dbname=web_service;charset=utf8', 'user', 'webservice');
+
+}catch( PDOException $Exception ) {
+    // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+    // String.
+
+    throw new MyDatabaseException( $Exception->getMessage( ) , $Exception->getCode( ) );
+}
+
+?>
+
+ 
