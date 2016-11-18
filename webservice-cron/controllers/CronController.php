a408aa02d8ea70c1983d1f620a2f08fd156b8672
Unknown <quentin.lejeune3@gmail.com>
2016-11-18 16:48:08 +0100

    push quentin

--- webservice-cron/controllers/CronController.php
+++ webservice-cron/controllers/CronController.php
@@ -4,18 +4,21 @@ class CronController extends MyController
 {
     public function getAction($request) {
         if(isset($request->url_elements[2])) {
-            $user_id = (int)$request->url_elements[2];
+            $task_id = (int)$request->url_elements[2];
             if(isset($request->url_elements[3])) {
                 switch($request->url_elements[3]) {
                 case '1':
-                    $data["message"] = "SELECT * FROM task";
+                    $data["message"] = [];
+                    foreach(BDD::$bdd->query("SELECT * FROM task") as $task){
+                      $data["message"][]=$task;
+                    }
                     break;
                 default:
                     // do nothing, this is not a supported action
                     break;
                 }
             } else {
-                $data["message"] = "here is the info for user " . $user_id;
+                $data["message"] = "here is the info for user " . $task_id;
             }
         } else {
             $data["message"] = "you want a list of users";
--- webservice-cron/library/bddconnect.php
+++ webservice-cron/library/bddconnect.php
@@ -1,8 +1,12 @@
 <?php
-
+Class BDD{
+  public static $bdd;
+  public static function connect(){
+    self::$bdd=new PDO('mysql:host=127.0.0.1;dbname=web_service;charset=utf8', 'user', 'webservice');
+  }
+}
 try{
-       $bdd = new PDO('mysql:host=192.168.1.199;dbname=web_service;charset=utf8', 'user', 'webservice');
-
+  BDD::connect();
 }catch( PDOException $Exception ) {
     // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
     // String.
